bb8e6070a7affa933d2fe0853c0fa44b23677b5c
Phibonoci <48280238+Phibonoci@users.noreply.github.com>
2019-03-18 13:14:24 +0300

    Update hash_map.h

--- hash_map.h
+++ hash_map.h
@@ -4,56 +4,66 @@
 #include <cstring>
 #include <functional>
 
-const size_t init_size = 256;
-const float ratio = 0.7;
+enum {
+    EMPTY = 0,
+    DELETED = 1,
+    USED = 2
+};
+
+const size_t INIT_SIZE = 256;
+const float INCREASE_RATIO = 2, DECREASE_RATIO = 7;
 
 template<class KeyType, class ValueType, class Hash = std::hash<KeyType>> class HashMap {
 private:
-    std::list<std::pair<const KeyType, ValueType>> l;
+    typedef std::list<std::pair<const KeyType, ValueType>> node_list;
+    node_list l;
     std::vector<char> deleted;
-    std::vector<typename std::list<std::pair<const KeyType, ValueType>>::iterator> table;
+    std::vector<typename node_list::iterator> table;
     std::conditional_t<std::is_function<Hash>::value, std::add_pointer_t<Hash>, Hash> hasher;
-    size_t N, table_size = 0;
+    size_t table_size = 0, elements_count = 0;
 
 public:
     typedef typename std::list<std::pair<const KeyType, ValueType>>::iterator iterator;
     typedef typename std::list<std::pair<const KeyType, ValueType>>::const_iterator const_iterator;
 
     HashMap(Hash hh = Hash()): hasher(hh) {
-        N = init_size;
-        table.resize(N);
-        deleted.resize(N);
+        table_size = INIT_SIZE;
+        table.resize(table_size);
+        deleted.resize(table_size);
     }
 
     HashMap(const HashMap& other) {
-        N = other.N;
-        table.resize(N);
-        deleted.resize(N);
+        table_size = other.table_size;
+        table.resize(table_size);
+        deleted.resize(table_size);
         hasher = other.hasher;
-        for (const auto &item : other)
+        for (const auto &item : other) {
             insert(item);
+        }
     }
 
     HashMap(const_iterator begin, const_iterator end, Hash hh = Hash()): hasher(hh) {
-        N = init_size;
-        table.resize(N);
-        deleted.resize(N);
+        table_size = INIT_SIZE;
+        table.resize(table_size);
+        deleted.resize(table_size);
 
-        while (begin != end)
+        while (begin != end) {
             insert(*begin++);
+        }
     }
 
     HashMap(std::initializer_list<std::pair<KeyType, ValueType>> init_list, Hash hh = Hash()): hasher(hh) {
-        N = std::max(init_size, init_list.size() * 2);
-        table.resize(N);
-        deleted.resize(N);
+        table_size = std::max(INIT_SIZE, init_list.size() * 2);
+        table.resize(table_size);
+        deleted.resize(table_size);
 
-        for (auto &item : init_list)
+        for (auto &item : init_list) {
             insert(item);
+        }
     }
 
     inline size_t size() const {
-        return table_size;
+        return elements_count;
     }
 
     bool empty() const {
@@ -81,15 +91,15 @@ public:
     }
 
     void reallocate(size_t size) {
-        N = size;
-        table.resize(N);
-        deleted.resize(N);
-        memset(deleted.data(), 0, N);
+        table_size = size;
+        table.resize(table_size);
+        deleted.resize(table_size);
+        memset(deleted.data(), 0, table_size);
         for (auto iter = l.begin(); iter != l.end(); ++iter) {
             for (int index = 0;; ++index) {
-                int k = (hasher(iter->first) + index) % N;
-                if (deleted[k] == 0 or deleted[k] == 1) {
-                    deleted[k] = 2;
+                int k = (hasher(iter->first) + index) % table_size;
+                if (deleted[k] == EMPTY || deleted[k] == DELETED) {
+                    deleted[k] = USED;
                     table[k] = iter;
                     break;
                 }
@@ -98,93 +108,101 @@ public:
     }
 
     inline void check() {
-        if (size() * 2 > N)
-            reallocate(N * 2);
-        else if (size() > init_size and size() * 7 < N)
-            reallocate(N / 2);
+        if (size() * INCREASE_RATIO > table_size)
+            reallocate(table_size * 2);
+        else if (size() > INIT_SIZE && size() * DECREASE_RATIO < table_size)
+            reallocate(table_size / 2);
     }
 
     void insert(const std::pair<KeyType, ValueType>& value) {
         size_t hash = hasher(value.first);
-        for (size_t i = 0; i < N; ++i) {
-            size_t index = (hash + i) % N;
-            if (deleted[index] == 0 || deleted[index] == 1) {
-                deleted[index] = 2;
+        for (size_t i = 0; i < table_size; ++i) {
+            size_t index = (hash + i) % table_size;
+            if (deleted[index] == EMPTY || deleted[index] == DELETED) {
+                deleted[index] = USED;
                 l.push_back(value);
                 table[index] = (--l.end());
                 check();
-                ++table_size;
+                ++elements_count;
                 return;
-            } else if (table[index]->first == value.first)
+            } else if (table[index]->first == value.first) {
                 return;
+            }
         }
     }
 
     void erase(const KeyType& key) {
         size_t hash = hasher(key);
-        for (size_t i = 0; i < N; ++i) {
-            size_t index = (hash + i) % N;
-            if (deleted[index] == 2 && table[index]->first == key) {
+        for (size_t i = 0; i < table_size; ++i) {
+            size_t index = (hash + i) % table_size;
+            if (deleted[index] == USED && table[index]->first == key) {
                 l.erase(table[index]);
-                deleted[index] = 1;
-                --table_size;
+                deleted[index] = DELETED;
+                --elements_count;
                 return;
             }
 
-            if (deleted[index] == 0)
+            if (deleted[index] == EMPTY) {
                 return;
+            }
         }
     }
 
     void clear() {
         for (auto it = l.begin(); it != l.end(); ++it) {
-            size_t index = hasher(it->first) % N;
+            size_t index = hasher(it->first) % table_size;
             while (true) {
-                if (deleted[index] == 0)
+                if (deleted[index] == EMPTY) {
                     break;
-                else
-                    deleted[index] = 0;
-                index = (index + 1) % N;
+                } else {
+                    deleted[index] = EMPTY;
+                }
+                index = (index + 1) % table_size;
             }
         }
-        table_size = 0;
+        elements_count = 0;
         l.clear();
     }
 
     iterator find(const KeyType& key) {
         size_t hash = hasher(key);
-        for (size_t i = 0; i < N; ++i) {
-            size_t index = (hash + i) % N;
-            if (deleted[index] == 2 && table[index]->first == key)
+        for (size_t i = 0; i < table_size; ++i) {
+            size_t index = (hash + i) % table_size;
+            if (deleted[index] == USED && table[index]->first == key) {
                 return table[index];
+            }
 
-            if (deleted[index] == 0)
+            if (deleted[index] == EMPTY) {
                 return l.end();
+            }
         }
         return l.end();
     }
 
     const_iterator find(const KeyType& key) const {
         size_t hash = hasher(key);
-        for (size_t i = 0; i < N; ++i) {
-            size_t index = (hash + i) % N;
-            if (deleted[index] == 2 && table[index]->first == key)
+        for (size_t i = 0; i < table_size; ++i) {
+            size_t index = (hash + i) % table_size;
+            if (deleted[index] == USED && table[index]->first == key) {
                 return table[index];
+            }
 
-            if (deleted[index] == 0)
+            if (deleted[index] == EMPTY) {
                 return end();
+            }
         }
         return end();
     }
 
     ValueType& operator[] (const KeyType& key) {
         size_t hash = hasher(key);
-        for (size_t i = 0; i < N; ++i) {
-            size_t index = (hash + i) % N;
-            if (deleted[index] == 2 && table[index]->first == key)
+        for (size_t i = 0; i < table_size; ++i) {
+            size_t index = (hash + i) % table_size;
+            if (deleted[index] == USED && table[index]->first == key) {
                 return table[index]->second;
+            }
 
-            if (deleted[index] == 0) {
+            if (deleted[index] == EMPTY) {
                 insert(std::make_pair(key, ValueType()));
                 return l.back().second;
             }
@@ -194,40 +212,45 @@ public:
 
     ValueType& at(const KeyType& key) {
         size_t hash = hasher(key);
-        for (size_t i = 0; i < N; ++i) {
-            size_t index = (hash + i) % N;
-            if (deleted[index] == 2 && table[index]->first == key)
+        for (size_t i = 0; i < table_size; ++i) {
+            size_t index = (hash + i) % table_size;
+            if (deleted[index] == USED && table[index]->first == key) {
                 return table[index]->second;
+            }
 
-            if (deleted[index] == 0)
+            if (deleted[index] == EMPTY) {
                 throw std::out_of_range("");
+            }
         }
         throw std::out_of_range("");
     }
 
     const ValueType& at(const KeyType& key) const {
         size_t hash = hasher(key);
-        for (size_t i = 0; i < N; ++i) {
-            size_t index = (hash + i) % N;
+        for (size_t i = 0; i < table_size; ++i) {
+            size_t index = (hash + i) % table_size;
 
-            if (deleted[index] == 2 && table[index]->first == key)
+            if (deleted[index] == USED && table[index]->first == key) {
                 return table[index]->second;
+            }
 
-            if (deleted[index] == 0)
+            if (deleted[index] == EMPTY) {
                 throw std::out_of_range("");
+            }
         }
         throw std::out_of_range("");
     }
 
     HashMap& operator = (const HashMap& other) {
         if (&other != this) {
-            N = other.N;
-            table.resize(N);
-            deleted.resize(N);
+            table_size = other.table_size;
+            table.resize(table_size);
+            deleted.resize(table_size);
             hasher = other.hasher;
 
-            for (const auto &item : other)
+            for (const auto &item : other) {
                 insert(item);
+            }
         }
 
         return *this;
